2624aa8245f78aa2773344b118333d0da039b48c
Roshka-Oleg <olegroshka.spy@gmail.com>
2025-10-22 23:30:26 +0300

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,7 +7,6 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-vh-100 bg-light">
-      {/* Навигационная панель */}
       <nav className="navbar navbar-dark bg-primary">
         <div className="container">
           <span className="navbar-brand mb-0 h1">
@@ -15,7 +14,6 @@ const Layout = ({ children }: LayoutProps) => {
           </span>
         </div>
       </nav>
-      {/* Основное содержимое */}
       {children}
     </div>
   );
