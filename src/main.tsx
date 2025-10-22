1e93b6b9feaf6b9215a887c9a7255233396750e3
Roshka-Oleg <olegroshka.spy@gmail.com>
2025-10-22 23:33:34 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-// Импортируем Bootstrap CSS для стилизации
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-/**
- * Точка входа в приложение
- * Используем React 18 API для рендеринга
- */
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
